78288261cf0b589dd87adc55be947145cb2cf4cc
Smit <saj39@njit.edu>
2022-05-16 14:23:02 -0400

    Added Database Connection to Server And created users table

---
+++ sql/001_create_table_users.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `Users` (
+    `id` INT NOT NULL AUTO_INCREMENT
+    ,`fname` VARCHAR(60) NOT NULL
+    ,`lname` VARCHAR(60) NOT NULL
+    ,`email` VARCHAR(100) NOT NULL
+    ,`username` VARCHAR(60) NOT NULL
+    ,`created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ,`is_active` TINYINT(1)
+    ,PRIMARY KEY(`id`)
+    ,UNIQUE (`email`)
+    ,UNIQUE (`username`)
+)
