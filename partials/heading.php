817a1e8099de8f35c40b1e1ce91363ffb254ce6b
Smit <saj39@njit.edu>
2022-05-18 17:10:46 -0400

    Added viewport in meta tag

--- partials/heading.php
+++ partials/heading.php
@@ -1,4 +1,8 @@
 <?php
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <h1><a href="https://smit-server/~smit/splitter_app/index.php">Splitter App</a></h1>
