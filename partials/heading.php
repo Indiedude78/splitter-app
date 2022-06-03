94998e782edb737126758e6217d25c1d3a575fdc
Smit <saj39@njit.edu>
2022-06-02 21:19:24 -0400

    Fixed heading link

--- partials/heading.php
+++ partials/heading.php
@@ -5,4 +5,4 @@ require_once(__DIR__ . "/../lib/helpers.php");
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<h1><a href="https://smit-server/~smit/splitter_app/index.php">Splitter App</a></h1>
+<h1><a href="index.php">Splitter App</a></h1>
